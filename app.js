e015f1b42d97e23b5104bbeb08de1c06aa9e8777
rupesh00mahat <103112257+rupesh00mahat@users.noreply.github.com>
2024-03-27 16:57:47 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 setInterval(function () {
   const daysLeft = document.querySelector(`.no-of-days`);
 
-  const feb9_2024 = new Date('2024-02-09');
-  const may19_2024 = new Date('2024-05-19');
+  const startDate = new Date();
+  const endDate = new Date('2024-07-05');
   
   // Calculate the difference in milliseconds
-  const differenceInMs = may19_2024 - feb9_2024;
+  const differenceInMs = endDate - startDate;
   
   // Convert milliseconds to days
   const differenceInDays = differenceInMs / (1000 * 60 * 60 * 24);
